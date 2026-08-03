b1b790e80f62116ac058e7c0c9cbc156d945cfc4
Shiven Garg <gargshiven046@gmail.com>
2026-08-03 17:31:37 +0530

    Added the html page along with App.jsx page and also fixed the package.json file\

---
+++ frontend/src/App.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Events from "./pages/Events";
+import Schedule from "./pages/Schedule";
+import Team from "./pages/Team";
+import Gallery from "./pages/Gallery";
+import Sponsors from "./pages/Sponsors";
+import Register from "./pages/Register";
+import Contact from "./pages/Contact";
+
+function App() {
+  return (
+    <Router>
+      <div className="min-h-screen flex flex-col">
+        <Navbar />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/events" element={<Events />} />
+            <Route path="/schedule" element={<Schedule />} />
+            <Route path="/team" element={<Team />} />
+            <Route path="/gallery" element={<Gallery />} />
+            <Route path="/sponsors" element={<Sponsors />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
